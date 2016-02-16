3a2a358247d70ead66f7db33cbbf2ebb86878e4f
nadavbh12 <nadavbh@gmail.com>
2016-02-16 17:15:24 +0200

    removed open_gl

--- nanoarch.c
+++ nanoarch.c
@@ -20,34 +20,34 @@ static void core_log(enum retro_log_level level, const char *fmt, ...);
 static bool video_set_pixel_format(unsigned format);
 static void video_refresh(const void *data, unsigned width, unsigned height, unsigned pitch);
 
-static GLFWwindow *g_win = NULL;
+//static GLFWwindow *g_win = NULL;
 //static snd_pcm_t *g_pcm = NULL;  // variable used for audio
-static float g_scale = 3;
+//static float g_scale = 3;
 
-static GLfloat g_vertex[] = {
-	-1.0f, -1.0f, // left-bottom
-	-1.0f,  1.0f, // left-top
-	 1.0f, -1.0f, // right-bottom
-	 1.0f,  1.0f, // right-top
-};
-static GLfloat g_texcoords[] ={
-	0.0f,  1.0f,
-	0.0f,  0.0f,
-	1.0f,  1.0f,
-	1.0f,  0.0f,
-};
+//static GLfloat g_vertex[] = {
+//	-1.0f, -1.0f, // left-bottom
+//	-1.0f,  1.0f, // left-top
+//	 1.0f, -1.0f, // right-bottom
+//	 1.0f,  1.0f, // right-top
+//};
+//static GLfloat g_texcoords[] ={
+//	0.0f,  1.0f,
+//	0.0f,  0.0f,
+//	1.0f,  1.0f,
+//	1.0f,  0.0f,
+//};
 
 // holds pixel/screen settings
 static struct {
-	GLuint tex_id;
-	GLuint pitch;
-	GLint tex_w, tex_h;
-	GLuint clip_w, clip_h;
-
-	GLuint pixfmt;
-	GLuint pixtype;
-
-	GLuint bpp;
+//	GLuint tex_id;
+//	GLuint pitch;
+//	GLint tex_w, tex_h;
+//	GLuint clip_w, clip_h;
+//
+//	GLuint pixfmt;
+//	GLuint pixtype;
+//
+//	GLuint bpp;
 
 	// nadav implementations below:
 	struct retro_game_geometry rGeom;
@@ -94,18 +94,18 @@ struct keymap {
 };
 
 struct keymap g_binds[] = {
-	{ GLFW_KEY_X, RETRO_DEVICE_ID_JOYPAD_A },
-	{ GLFW_KEY_Z, RETRO_DEVICE_ID_JOYPAD_B },
-	{ GLFW_KEY_A, RETRO_DEVICE_ID_JOYPAD_Y },
-	{ GLFW_KEY_S, RETRO_DEVICE_ID_JOYPAD_X },
-	{ GLFW_KEY_UP, RETRO_DEVICE_ID_JOYPAD_UP },
-	{ GLFW_KEY_DOWN, RETRO_DEVICE_ID_JOYPAD_DOWN },
-	{ GLFW_KEY_LEFT, RETRO_DEVICE_ID_JOYPAD_LEFT },
-	{ GLFW_KEY_RIGHT, RETRO_DEVICE_ID_JOYPAD_RIGHT },
-	{ GLFW_KEY_ENTER, RETRO_DEVICE_ID_JOYPAD_START },
-	{ GLFW_KEY_BACKSPACE, RETRO_DEVICE_ID_JOYPAD_SELECT },
-
-	{ 0, 0 }
+//	{ GLFW_KEY_X, RETRO_DEVICE_ID_JOYPAD_A },
+//	{ GLFW_KEY_Z, RETRO_DEVICE_ID_JOYPAD_B },
+//	{ GLFW_KEY_A, RETRO_DEVICE_ID_JOYPAD_Y },
+//	{ GLFW_KEY_S, RETRO_DEVICE_ID_JOYPAD_X },
+//	{ GLFW_KEY_UP, RETRO_DEVICE_ID_JOYPAD_UP },
+//	{ GLFW_KEY_DOWN, RETRO_DEVICE_ID_JOYPAD_DOWN },
+//	{ GLFW_KEY_LEFT, RETRO_DEVICE_ID_JOYPAD_LEFT },
+//	{ GLFW_KEY_RIGHT, RETRO_DEVICE_ID_JOYPAD_RIGHT },
+//	{ GLFW_KEY_ENTER, RETRO_DEVICE_ID_JOYPAD_START },
+//	{ GLFW_KEY_BACKSPACE, RETRO_DEVICE_ID_JOYPAD_SELECT },
+//
+//	{ 0, 0 }
 };
 
 static unsigned g_joy[RETRO_DEVICE_ID_JOYPAD_R3+1] = { 0 };
@@ -135,70 +135,70 @@ static void die(const char *fmt, ...) {
 }
 
 // move window??
-static void refresh_vertex_data() {
-	assert(g_video.tex_w);
-	assert(g_video.tex_h);
-	assert(g_video.clip_w);
-	assert(g_video.clip_h);
-
-	GLfloat *coords = g_texcoords;
-	coords[1] = coords[5] = (float)g_video.clip_h / g_video.tex_h;
-	coords[4] = coords[6] = (float)g_video.clip_w / g_video.tex_w;
-}
+//static void refresh_vertex_data() {
+//	assert(g_video.tex_w);
+//	assert(g_video.tex_h);
+//	assert(g_video.clip_w);
+//	assert(g_video.clip_h);
+//
+//	GLfloat *coords = g_texcoords;
+//	coords[1] = coords[5] = (float)g_video.clip_h / g_video.tex_h;
+//	coords[4] = coords[6] = (float)g_video.clip_w / g_video.tex_w;
+//}
 
 // resize windows
-static void resize_cb(GLFWwindow *win, int w, int h) {
-	glViewport(0, 0, w, h);
-}
+//static void resize_cb(GLFWwindow *win, int w, int h) {
+//	glViewport(0, 0, w, h);
+//}
 
 
-static void create_window(int width, int height) {
-	// set window properties
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-	// create window
-	g_win = glfwCreateWindow(width, height, "nanoarch", NULL, NULL);
-
-	if (!g_win)
-		die("Failed to create window.");
-
-	// set the function which is called when the window is resized
-	glfwSetFramebufferSizeCallback(g_win, resize_cb);
-
-	// make the thread of the window current ?
-	glfwMakeContextCurrent(g_win);
-
-	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK)
-		die("Failed to initialize glew");
-
-	// sets how many screen updates before a contex switch will occur
-	glfwSwapInterval(1);
-
-	printf("GLSL Version: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
-
-	glEnable(GL_TEXTURE_2D);
-
-//	refresh_vertex_data();
-
-	resize_cb(g_win, width, height);
-}
+//static void create_window(int width, int height) {
+//	// set window properties
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+//	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+//
+//	// create window
+//	g_win = glfwCreateWindow(width, height, "nanoarch", NULL, NULL);
+//
+//	if (!g_win)
+//		die("Failed to create window.");
+//
+//	// set the function which is called when the window is resized
+//	glfwSetFramebufferSizeCallback(g_win, resize_cb);
+//
+//	// make the thread of the window current ?
+//	glfwMakeContextCurrent(g_win);
+//
+//	glewExperimental = GL_TRUE;
+//	if (glewInit() != GLEW_OK)
+//		die("Failed to initialize glew");
+//
+//	// sets how many screen updates before a contex switch will occur
+//	glfwSwapInterval(1);
+//
+//	printf("GLSL Version: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+//
+//	glEnable(GL_TEXTURE_2D);
+//
+////	refresh_vertex_data();
+//
+//	resize_cb(g_win, width, height);
+//}
 
 // sets dw,dh (destination) according to the ration and sw,sh (source)
-static void resize_to_aspect(double ratio, int sw, int sh, int *dw, int *dh) {
-	*dw = sw;
-	*dh = sh;
-
-	if (ratio <= 0)
-		ratio = (double)sw / sh;
-
-	if ((float)sw / sh < 1)
-		*dw = *dh * ratio;
-	else
-		*dh = *dw / ratio;
-}
+//static void resize_to_aspect(double ratio, int sw, int sh, int *dw, int *dh) {
+//	*dw = sw;
+//	*dh = sh;
+//
+//	if (ratio <= 0)
+//		ratio = (double)sw / sh;
+//
+//	if ((float)sw / sh < 1)
+//		*dw = *dh * ratio;
+//	else
+//		*dh = *dw / ratio;
+//}
 
 static size_t audio_write(const void *buf, unsigned frames) {
 //	int written = snd_pcm_writei(g_pcm, buf, frames);
@@ -269,32 +269,6 @@ static void video_refresh(const void *data, unsigned width, unsigned height, uns
 		g_video.rGeom.base_height = height;
 		g_video.rGeom.base_width  = width;
 	}
-//
-//	uint32_t inBpp = pitch/width;
-//	if(g_video.nbpp != inBpp ){
-//		g_video.nbpp = inBpp;
-//	}
-
-//	 first attempt by inserting each pixel
-//	SDL_Surface* screen = SDL_CreateRGBSurface(SDL_SWSURFACE, g_video.rGeom.base_width,
-//						g_video.rGeom.base_height,g_video.nbpp,
-//						g_video.rmask, g_video.gmask, g_video.bmask, g_video.amask);
-//
-//    if ( SDL_MUSTLOCK(screen) ) {
-//        if ( SDL_LockSurface(screen) < 0 ) {
-//            fprintf(stderr, "Can't lock screen: %s\n", SDL_GetError());
-//            return;
-//        }
-//    }
-//	Uint8 r, g, b;
-//	for (int y=0; y<g_video.rGeom.base_height; ++y) {
-//		for (int x=0; x<g_video.rGeom.base_width; ++x) {
-//			Uint32 *  bufp = (Uint32 *)screen->pixels + y*screen->pitch + x*screen->format->BytesPerPixel;
-//			SDL_GetRGB(bufp, screen->format, r, g, b);
-//			pixelRGBA(screen,x,y,r,g,b,255);
-//		}
-//	}
-
 	SDL_Surface * screen = SDL_CreateRGBSurfaceFrom(data, g_video.rGeom.base_width, g_video.rGeom.base_height,
 								g_video.nbpp, pitch, g_video.rmask, g_video.gmask, g_video.bmask, g_video.amask);
 	SDL_Surface* zoomed = zoomSurface(screen, g_video.rGeom.base_width/(double)width, g_video.rGeom.base_height/(double)height, 0);
@@ -308,26 +282,26 @@ static void video_refresh(const void *data, unsigned width, unsigned height, uns
     SDL_FreeSurface(zoomed);
 
 	// ******* end
-	if (g_video.clip_w != width || g_video.clip_h != height) {
-		g_video.clip_h = height;
-		g_video.clip_w = width;
-
-		refresh_vertex_data();
-	}
-
-	glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
-
-	// update pitch if necessary
-	if (pitch != g_video.pitch) {
-		g_video.pitch = pitch;
-		glPixelStorei(GL_UNPACK_ROW_LENGTH, g_video.pitch / g_video.bpp);
-	}
-
-	// display new image
-	if (data) {
-		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
-						g_video.pixtype, g_video.pixfmt, data);
-	}
+//	if (g_video.clip_w != width || g_video.clip_h != height) {
+//		g_video.clip_h = height;
+//		g_video.clip_w = width;
+//
+//		refresh_vertex_data();
+//	}
+//
+//	glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
+//
+//	// update pitch if necessary
+//	if (pitch != g_video.pitch) {
+//		g_video.pitch = pitch;
+//		glPixelStorei(GL_UNPACK_ROW_LENGTH, g_video.pitch / g_video.bpp);
+//	}
+//
+//	// display new image
+//	if (data) {
+//		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
+//						g_video.pixtype, g_video.pixfmt, data);
+//	}
 }
 
 
@@ -360,8 +334,6 @@ static void video_configure(const struct retro_game_geometry *geom) {
     if(!g_video.nbpp){
     	g_video.nbpp = 16;
     }
-    printf("g_video.nbpp : %d\n", g_video.nbpp );
-    printf("g_video.bpp : %d\n", g_video.bpp );
 
     // TODO make sure we update the bpp
     g_video.screen = SDL_SetVideoMode(geom->max_width, geom->max_height, g_video.nbpp, SDL_SWSURFACE);
@@ -371,84 +343,84 @@ static void video_configure(const struct retro_game_geometry *geom) {
     }
 	// ******* end
 
-	int nwidth, nheight;
-
-	resize_to_aspect(geom->aspect_ratio, geom->base_width * 1, geom->base_height * 1, &nwidth, &nheight);
-
-	nwidth *= g_scale;
-	nheight *= g_scale;
-
-	if (!g_win)
-		create_window(nwidth, nheight);
-
-	if (g_video.tex_id)
-		glDeleteTextures(1, &g_video.tex_id);
-
-	g_video.tex_id = 0;
-
-	// set default pixel format
-	if (!g_video.pixfmt)
-		g_video.pixfmt = GL_UNSIGNED_SHORT_5_5_5_1;
-
-	// resize window
-	glfwSetWindowSize(g_win, nwidth, nheight);
-
-	// set window format/texture
-	glGenTextures(1, &g_video.tex_id);
-
-	if (!g_video.tex_id)
-		die("Failed to create the video texture");
-
-	// save the video's pitch
-	g_video.pitch = geom->base_width * g_video.bpp;
-
-	glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
-
-//	glPixelStorei(GL_UNPACK_ALIGNMENT, s_video.pixfmt == GL_UNSIGNED_INT_8_8_8_8_REV ? 4 : 2);
-//	glPixelStorei(GL_UNPACK_ROW_LENGTH, s_video.pitch / s_video.bpp);
-
-	// set texture parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, geom->max_width, geom->max_height, 0,
-			g_video.pixtype, g_video.pixfmt, NULL);
-
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	// save the width, height
-	g_video.tex_w = geom->max_width;
-	g_video.tex_h = geom->max_height;
-	g_video.clip_w = geom->base_width;
-	g_video.clip_h = geom->base_height;
-
-	refresh_vertex_data();
+//	int nwidth, nheight;
+//
+//	resize_to_aspect(geom->aspect_ratio, geom->base_width * 1, geom->base_height * 1, &nwidth, &nheight);
+//
+//	nwidth *= g_scale;
+//	nheight *= g_scale;
+//
+//	if (!g_win)
+//		create_window(nwidth, nheight);
+//
+//	if (g_video.tex_id)
+//		glDeleteTextures(1, &g_video.tex_id);
+//
+//	g_video.tex_id = 0;
+//
+//	// set default pixel format
+//	if (!g_video.pixfmt)
+//		g_video.pixfmt = GL_UNSIGNED_SHORT_5_5_5_1;
+//
+//	// resize window
+//	glfwSetWindowSize(g_win, nwidth, nheight);
+//
+//	// set window format/texture
+//	glGenTextures(1, &g_video.tex_id);
+//
+//	if (!g_video.tex_id)
+//		die("Failed to create the video texture");
+//
+//	// save the video's pitch
+//	g_video.pitch = geom->base_width * g_video.bpp;
+//
+//	glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
+//
+////	glPixelStorei(GL_UNPACK_ALIGNMENT, s_video.pixfmt == GL_UNSIGNED_INT_8_8_8_8_REV ? 4 : 2);
+////	glPixelStorei(GL_UNPACK_ROW_LENGTH, s_video.pitch / s_video.bpp);
+//
+//	// set texture parameters
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+//
+//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, geom->max_width, geom->max_height, 0,
+//			g_video.pixtype, g_video.pixfmt, NULL);
+//
+//	glBindTexture(GL_TEXTURE_2D, 0);
+//
+//	// save the width, height
+//	g_video.tex_w = geom->max_width;
+//	g_video.tex_h = geom->max_height;
+//	g_video.clip_w = geom->base_width;
+//	g_video.clip_h = geom->base_height;
+//
+//	refresh_vertex_data();
 }
 
 // sets the pixel format as requested by the core
 static bool video_set_pixel_format(unsigned format) {
-	if (g_video.tex_id)
-		die("Tried to change pixel format after initialization.");
-
-	switch (format) {
-	case RETRO_PIXEL_FORMAT_0RGB1555:
-		g_video.pixfmt = GL_UNSIGNED_SHORT_1_5_5_5_REV;
-		g_video.pixtype = GL_BGRA;
-		g_video.bpp = sizeof(uint16_t);
-		break;
-	case RETRO_PIXEL_FORMAT_XRGB8888:
-		g_video.pixfmt = GL_UNSIGNED_INT_8_8_8_8_REV;
-		g_video.pixtype = GL_BGRA;
-		g_video.bpp = sizeof(uint32_t);
-		break;
-	case RETRO_PIXEL_FORMAT_RGB565:
-		g_video.pixfmt  = GL_UNSIGNED_SHORT_5_6_5;
-		g_video.pixtype = GL_RGB;
-		g_video.bpp = sizeof(uint16_t);
-		break;
-	default:
-		die("Unknown pixel type %u", format);
-	}
+//	if (g_video.tex_id)
+//		die("Tried to change pixel format after initialization.");
+//
+//	switch (format) {
+//	case RETRO_PIXEL_FORMAT_0RGB1555:
+//		g_video.pixfmt = GL_UNSIGNED_SHORT_1_5_5_5_REV;
+//		g_video.pixtype = GL_BGRA;
+//		g_video.bpp = sizeof(uint16_t);
+//		break;
+//	case RETRO_PIXEL_FORMAT_XRGB8888:
+//		g_video.pixfmt = GL_UNSIGNED_INT_8_8_8_8_REV;
+//		g_video.pixtype = GL_BGRA;
+//		g_video.bpp = sizeof(uint32_t);
+//		break;
+//	case RETRO_PIXEL_FORMAT_RGB565:
+//		g_video.pixfmt  = GL_UNSIGNED_SHORT_5_6_5;
+//		g_video.pixtype = GL_RGB;
+//		g_video.bpp = sizeof(uint16_t);
+//		break;
+//	default:
+//		die("Unknown pixel type %u", format);
+//	}
 
 	// nadav
 	switch (format) {
@@ -504,10 +476,11 @@ static bool video_set_pixel_format(unsigned format) {
 
 
 static void video_deinit() {
-	if (g_video.tex_id)
-		glDeleteTextures(1, &g_video.tex_id);
+//	if (g_video.tex_id)
+//		glDeleteTextures(1, &g_video.tex_id);
 
-	g_video.tex_id = 0;
+//	g_video.tex_id = 0;
+
 }
 
 
@@ -529,9 +502,9 @@ static size_t core_audio_sample_batch(const int16_t *data, size_t frames) {
 
 // reads for input from keyboard
 static void core_input_poll(void) {
-	int i;
-	for (i = 0; g_binds[i].k || g_binds[i].rk; ++i)
-		g_joy[g_binds[i].rk] = (glfwGetKey(g_win, g_binds[i].k) == GLFW_PRESS);
+//	int i;
+//	for (i = 0; g_binds[i].k || g_binds[i].rk; ++i)
+//		g_joy[g_binds[i].rk] = (glfwGetKey(g_win, g_binds[i].k) == GLFW_PRESS);
 }
 
 // returns for every key whether it is pressed or not
@@ -660,46 +633,53 @@ static void core_unload() {
 }
 
 
-static void video_render() {
-	glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
-
-	// unlock the array for writing
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
-	glVertexPointer(2, GL_FLOAT, 0, g_vertex);
-	glTexCoordPointer(2, GL_FLOAT, 0, g_texcoords);
-
-	// Specifies what kind of primitives to render
-	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-}
+//static void video_render() {
+//	glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
+//
+//	// unlock the array for writing
+//	glEnableClientState(GL_VERTEX_ARRAY);
+//	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+//
+//	glVertexPointer(2, GL_FLOAT, 0, g_vertex);
+//	glTexCoordPointer(2, GL_FLOAT, 0, g_texcoords);
+//
+//	// Specifies what kind of primitives to render
+//	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+//}
 
 int main(int argc, char *argv[]) {
 	if (argc < 3)
 		die("usage: %s <core> <game>", argv[0]);
 
-	if (!glfwInit())
-		die("Failed to initialize glfw");
+//	if (!glfwInit())
+//		die("Failed to initialize glfw");
 
 	core_load(argv[1]);
 	core_load_game(argv[2]);
 
-	while (!glfwWindowShouldClose(g_win)) {
-		glfwPollEvents();
+	SDL_Event event;
+//	while (!glfwWindowShouldClose(g_win)) {
+	while (SDL_PollEvent(&event)) {
+//		glfwPollEvents();
+
+		if(event.type == SDL_KEYDOWN){
+			if(event.key.keysym.sym == SDLK_ESCAPE)
+				SDL_Quit();
+		}
 
 		g_retro.retro_run();
 
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		video_render();
-
-		glfwSwapBuffers(g_win);
+//		glClear(GL_COLOR_BUFFER_BIT);
+//
+//		video_render();
+//
+//		glfwSwapBuffers(g_win);
 	}
 
 	core_unload();
 	audio_deinit();
 	video_deinit();
 
-	glfwTerminate();
+//	glfwTerminate();
 	return 0;
 }
